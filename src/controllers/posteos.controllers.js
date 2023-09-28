4284e631fa0107e28a55f8a99003fb441a50893e
Fede <fede.tortolero@gmail.com>
2023-09-28 12:24:55 -0300

    tratando de crear menu de edicion

--- src/controllers/posteos.controllers.js
+++ src/controllers/posteos.controllers.js
@@ -10,7 +10,7 @@ const createPosteo = async (req, res) => {
 };
 
 const updatePosteo = async (req, res) => {
-  const posteoId = req.params.id;
+  const posteoId = req.body.posteoId;
   const { title, content, image } = req.body;
 
   const posteo = await PosteoModel.findByPk(posteoId);
@@ -26,4 +26,32 @@ const listPosteos = async (req, res) => {
   res.json(allPosteos);
 };
 
-module.exports = { createPosteo, listPosteos, updatePosteo };
+const mostrarFormularioEdicion = async (req, res) => {
+  try {
+    // Obtén el ID del posteo seleccionado desde la solicitud
+    const posteoId = req.query.posteoId;
+
+    // Busca el posteo seleccionado por ID
+    const posteo = await PosteoModel.findByPk(posteoId);
+
+    if (!posteo) {
+      // Maneja el caso en el que el posteo no se encuentra
+      res.status(404).send("No se encontró el posteo para editar.");
+      return;
+    }
+
+    // Obtener la lista de todos los posteos para mostrarla en el formulario
+    const listaDePosteos = await PosteoModel.findAll();
+
+    res.render("edit", {
+      title: "Editar Posteo",
+      posteo,
+      listaDePosteos,
+    });
+  } catch (error) {
+    console.error("Error al mostrar el formulario de edición:", error);
+    res.status(500).send("Error interno del servidor");
+  }
+};
+
+module.exports = { createPosteo, listPosteos, updatePosteo, mostrarFormularioEdicion };
--- src/routes/posteos.routers.js
+++ src/routes/posteos.routers.js
@@ -3,12 +3,14 @@ const {
     createPosteo,
     listPosteos,
     updatePosteo,
+    mostrarFormularioEdicion,
 } = require("../controllers/posteos.controllers");
 
 const router = Router();
 
 router.get("/", listPosteos);
 router.post("/", createPosteo);
+router.get("/edit/:id", mostrarFormularioEdicion);
 router.post("/:id", updatePosteo);
 
 module.exports = router;
